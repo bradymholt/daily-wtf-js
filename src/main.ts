86d8b8b889e69881dd57811180637c2a63d71d99
Brady Holt <brady.holt@gmail.com>
2022-12-14 14:24:08 -0700

    Use prismjs for code formatting

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
 import "./style.css";
+
+import "prismjs/themes/prism.css";
+import Prism from "prismjs";
+Prism.manual = true;
+
 const modules = import.meta.glob("./wtfs/*.html", { as: "raw" });
 const moduleFileNames = Object.keys(modules);
 const randomWtfFileName = moduleFileNames[Math.floor(Math.random() * moduleFileNames.length)];
 modules[randomWtfFileName]().then((randomWtfContent) => {
   document.querySelector<HTMLDivElement>("#wtf")!.innerHTML = randomWtfContent;
+  Prism.highlightAll();
 });
